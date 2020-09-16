b9af10b9eb0a3d5adf12e7b9fa42829a77e08851
liudonghua123 <liudonghua123@gmail.com>
2020-09-16 09:03:41 +0800

    initial package via stagehand package-simple

---
+++ example/simple_throttle_debounce_example.dart
@@ -0,0 +1,6 @@
+import 'package:simple_throttle_debounce/simple_throttle_debounce.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/simple_throttle_debounce.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library simple_throttle_debounce;
+
+export 'src/simple_throttle_debounce_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
